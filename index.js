55a24c3db982bd6bc93906bee8ede3a6e1a91c95
CatmullJason <jasoncatmull@gmail.com>
2017-11-28 14:36:12 -0700

    trying a new upload

--- index.js
+++ index.js
@@ -2,45 +2,145 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var url = require('url');
+var cors = require('cors');
 
 var pg = require("pg"); // This is the postgres database connection module.
-//const connectionString = "postgres://postgres:Arsenal1!@localhost:5432/tickets";
+//const connectionString = "postgres://postgres:Arsenal1!@localhost:5432/isotopedb";
 const connectionString = "postgres://gpdqeokklqlkaw:55559494c7673658470f8e816cd8615ead67cff1ca62dc27b41ee4c2d4a3c239@ec2-107-21-205-25.compute-1.amazonaws.com:5432/dd8kbik4stv9se";
 app.set('port', (process.env.PORT || 5000));
 
+//static page setup
 app.use(express.static(__dirname + '/public'));
+
+//cors
+app.use(cors())
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/getIsotopes', function (request, response) {
-    getIsotopes(request, response);
-
+//listen for port
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('/calculateKilos', function (request, response){
+
+/************************************
+* SERVICE ENDPOINT:
+* Get list of isotopes from server database
+*************************************/
+app.get('/getIsotopes', function (request, response) {
+    getIsotopes(request, response);
+});
+
+/************************************
+* SERVICE ENDPOINT:
+* Calculate Decayed Activity
+*************************************/
+app.get('/calculateDecay', function (request, response) {
     var requestUrl = url.parse(request.url, true);
-    
+
+    console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+
+    //isotope
+    var isotope = String(requestUrl.query.isotope);
+
+    //time
+    var time = parseFloat(requestUrl.query.time);
+
+    //calculate BMI
+    var activity = parseFloat(requestUrl.query.activity);
+
+    //half life
+    var halfLife = assignHalfLife(isotope);
+
+    //calculation
+    var decayedActivity = findDecayedActivity(halfLife, activity, time) + " mCi";
+
+    var params = { decayedActivity: decayedActivity };
+
+    response.status(200).json(params);
+});
+
+//calculateDecay helper function
+function assignHalfLife(isotope) {
+    switch (isotope) {
+        case "F-18":
+            return halfLife = 1.83;
+            break;
+        case "Tc-99m":
+            return halfLife = 6.05;
+            break;
+        case "I-131":
+            return halfLife = 192.48;
+            break;
+        case "I-123":
+            return halfLife = 13.22;
+            break;
+        default:
+            alert("error");
+    }
+    return;
+}
+
+//calculateDecay helper function
+function findDecayedActivity(halfLife, activity, time) {
+    var stepZero = time * -0.693;
+    var stepOne = stepZero / halfLife;
+    var stepTwo = Math.exp(stepOne);
+    var stepThree = activity * stepTwo;
+
+    return stepThree.toFixed(2);
+}
+
+/************************************
+* SERVICE ENDPOINT:
+* Calculate BMI
+*************************************/
+app.get('/calculateBMI', function (request, response) {
+    var requestUrl = url.parse(request.url, true);
+
+    console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+
+    //weight
+    var kgs = requestUrl.query.kgs;
+
+    //height
+    var feetInInches = requestUrl.query.feet * 12;
+
+    var heightInInches = (+requestUrl.query.inches) + (+feetInInches);
+
+    var heightInMeters = heightInInches.toFixed(2) * .0254;
+
+    //calculate BMI
+    var bmi = parseFloat((kgs / heightInMeters) / heightInMeters).toFixed(2);
+
+    var params = { bmi: bmi };
+
+    response.status(200).json(params);
+});
+
+/************************************
+* SERVICE ENDPOINT:
+* Calculate Pounds from given Kilos
+*************************************/
+app.get('/calculateKilos', function (request, response) {
+    var requestUrl = url.parse(request.url, true);
+
     console.log("Query parameters: " + JSON.stringify(requestUrl.query));
 
     var kgs = requestUrl.query.kgs;
 
-    var pounds = calculateKilos(kgs);
+    var pounds = kgs * 2.2;
 
     var params = { pounds: pounds };
 
     response.status(200).json(params);
 });
 
-function calculateKilos(pounds) {
-    return pounds * 2.2;
-}
-
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
-
+/*************************************
+* GET ISOTOPES FUNCTION
+**************************************/
 function getIsotopes(request, response) {
 
     getIsotopesFromDB(function (error, result) {
@@ -53,11 +153,18 @@ function getIsotopes(request, response) {
     });
 }
 
+/*************************************
+* GET ISOTOPES FROM DB
+* Create connection to database and then pull 
+* values from the isotope table
+**************************************/
 function getIsotopesFromDB(callback) {
     console.log("Getting isotopes from DB");
 
+    //create new client object with connection string
     var client = new pg.Client(connectionString);
 
+    //connect to server DB
     client.connect(function (err) {
         if (err) {
             console.log("Error connecting to DB: ")
@@ -65,8 +172,10 @@ function getIsotopesFromDB(callback) {
             callback(err, null);
         }
 
+        //get everything from table
         var sql = "SELECT * FROM isotope";
 
+        //query string
         var query = client.query(sql, function (err, result) {
             // we are now done getting the data from the DB, disconnect the client
             client.end(function (err) {
--- public/script.js
+++ public/script.js
@@ -1,11 +1,10 @@
-$(document).ready( function(){
+$(document).ready(function () {
     $.get("/getIsotopes", function (result) {
 
         var html = '';
-        for (var i = 0; i < result.length; i++)
-        {
-            html += '<tr><td>' + result[i].name + 
-            '</td><td>' + result[i].halflife + '</td></tr>';
+        for (var i = 0; i < result.length; i++) {
+            html += '<tr><td>' + result[i].name +
+                '</td><td>' + result[i].halflife + ' minutes</td></tr>';
         }
 
         $('#isotopeTable tr').first().after(html);
@@ -18,4 +17,50 @@ function calculateKilos() {
     $.get("/calculateKilos?kgs=" + kgs, function (result) {
         document.getElementById('lbsoutput').value = Math.round(result.pounds);
     });
+}
+
+function calculateDecay() {
+    var selectedIsotope = document.getElementById("isoID");
+    var isotope = selectedIsotope.options[selectedIsotope.selectedIndex].value;
+    var timePassed = document.getElementById("timePassed").value;
+    var currentActivity = document.getElementById("currentActivity").value;
+    
+    $.get("/calculateDecay?isotope=" + isotope + "&time=" + timePassed + "&activity=" + currentActivity,
+            function (result) {
+                document.getElementById("decayResult").value = result.decayedActivity;
+            })
+}
+
+function calculateBMI() {
+
+    var kgs = document.getElementById('weightKilos').value;
+    var feet = document.getElementById('heightFeet').value;
+    var inches = document.getElementById('heightInches').value;
+
+    $.get("/calculateBMI?kgs=" + kgs + "&feet=" + feet + "&inches=" + inches, function(result) {
+        document.getElementById('bmiResult').value = result.bmi;
+    })
+}
+
+function searchMedline() {
+    var userInput = $('#medicalCondition').val();
+    $.ajax({
+        type: 'GET',
+        url: "https://wsearch.nlm.nih.gov/ws/query?db=healthTopics&term=" + userInput,
+
+        dataType: "jsonp",
+        jsonp: true,
+        crossDomain: true,
+        withCredentials: true,
+        success: function (responseData, textStatus, jqXHR) {
+
+            alert("success");
+
+        },
+        error: function (responseData, textStatus, errorThrown) {
+            alert('POST failed.');
+        }
+    });
+
+
 }
